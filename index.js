9b1e521b52cc6c7d9da72e585821927e5f951540
Ryan Sweeney <rasweeney94@yahoo.com>
2019-01-14 17:32:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 var kittens = [] //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(name) {
+  
+}
+
+function destructivelyPrependKitten(name) {
+  
+}
+
+function destructivelyRemoveLastKitten() {
+  
+}
+
+function destructivelyRemoveFirstKitten() {
+  
+}
+
+function appendKitten(name) {
+  
+}
+
+function prependKitten(name) {
+  
+}
+
+function removeLastKitten() {
+  
+}
+
+function removeFirstKitten() {
+  
+}
